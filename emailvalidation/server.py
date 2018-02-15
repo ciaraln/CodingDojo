017e52c010f4125e89daecadad029d4d9bf7a6ff
Ciara L N <ciara.l.nelson@live.com>
2018-02-15 10:59:00 -0800

    email validation
    need to run again for complete access

--- emailvalidation/server.py
+++ emailvalidation/server.py
@@ -1,31 +1,59 @@
 from flask import Flask, request, redirect, render_template, session, flash
+
 from mysqlconnection import MySQLConnector
-import re 
-emailregex = re.compile(r'^[a-zA-Z0-9.+-]+@[_a-zA-Z0-9.+-]+.[a-zA-Z]+$')
+
+import re
+
+emailregex = re.compile(r'^[a-zA-Z0-9.+-]+@[a-zA-Z0-9.+-]+.[a-zA-Z]+$')
 
 app = Flask(__name__)
-app.secret_key = 'supertopsecretsquire'
+
+app.secret_key = 'supertopsecret'
+
 mysql = MySQLConnector(app,'emails')
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/email', methods=['POST'])
-def createEmail():
-	email= request.form['email']
+
+@app.route('/')
+
+def index():
+
+	return render_template('index.html')
+
+	
+
+@app.route('/process', methods=['POST'])
+
+def new_email():
+
+	email = request.form['email']
+
 	if not emailregex.match(email):
-		flash('Not a valid email, check again!')
+
+		flash("Not a valid email!")
+
 	if emailregex.match(email):
+
 		checkQuery = mysql.query_db('SELECT * FROM emails WHERE email_address = "{}"'.format(email))
+
 		if len(checkQuery) > 0:
-			flash('This email already exists.')
+
+			flash("This email already exists.")
+
 		else:
+
 			email_query = ('INSERT INTO emails (email_address, created_at, updated_at) VALUES ("{}", NOW(), NOW())'.format(email))
+
 			mysql.query_db(email_query)
+
 			query = "SELECT * FROM emails"
+
 			emails = mysql.query_db(query)
-			flash("The email_address you entered ({}) is VALID email address! Thank You!".format(email))
-			return render_template('success.html', all_emails= emails)
+
+			flash("The email address you entered ({})is a VALID email address! Thank you!".format(email))
+
+			return render_template('success.html', all_emails=emails)
+
+	
 
 app.run(debug=True)
